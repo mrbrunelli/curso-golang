a8ac67056826ffb98284b657a00b93afce3cfda7
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-01-06 19:56:43 -0300

    add lista de comando úteis

---
+++ fundamentos/comandos/comandos.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	gl := "Golang"
+	fmt.Printf("Outro programa em %s!", gl)
+}
+
+/*
+Instalar pacotes
+go get -u github.com/go-sql-driver/mysql
+
+Ler documentação de algum comando ou funcão
+go doc fmt
+
+Ver variáveis de ambiente
+go env
+
+Ajuda sobre comandos gerais, ou específicos
+go help
+ou
+go help fmt
+
+Verificar se existe algum erro no código
+go vet
+*/
